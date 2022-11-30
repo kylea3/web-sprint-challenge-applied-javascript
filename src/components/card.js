c9e6191555390fc0c9fad551e9b91239bb6bd71a
Kyle Andrus <kyleandrus3@gmail.com>
2022-11-30 14:55:16 -0700

    complete task 5

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,28 @@
 const Card = (article) => {
+  const card = document.createElement('div');
+  const headline = document.createElement('div');
+  const author = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const img = document.createElement('img');
+  const authorName = document.createElement('span');
+  
+  card.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  imgContainer.classList.add('img-container');
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  author.appendChild(authorName);
+  imgContainer.appendChild(img);
+
+  headline.textContent = article.headline;
+  img.src = article.authorPhoto;
+  authorName.textContent = `By ${article.authorName}`
+
+  return card;
+
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
